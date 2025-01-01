d2d5ac1748d700771871909ca99145144cdbbef0
Nicolas <nicolas.programador@gmail.com>
2025-01-01 20:28:05 -0300

    EditarLibro Ok

--- src/main/java/com/example/bibliotecaduoc/repository/LibroRepository.java
+++ src/main/java/com/example/bibliotecaduoc/repository/LibroRepository.java
@@ -37,4 +37,35 @@ public class LibroRepository {
         return null;
     }
 
+
+    //Guardar Libro
+    public Libro guardar(Libro lib){
+        listaLibros.add(lib);
+        return lib;
+    }
+
+    //Actualizar Libro
+    public Libro actualizar(Libro lib){
+        int id = 0;
+        int idPosicion = 0;
+
+        for(int i = 0;i<listaLibros.size();i++){
+            if (listaLibros.get(i).getId() == lib.getId()) {
+                id = lib.getId();
+                idPosicion = i;
+            }
+        }
+
+        Libro libro1 = new Libro();
+        libro1.setId(id);
+        libro1.setTitulo(lib.getTitulo());
+        libro1.setAutor(lib.getAutor());
+        libro1.setFechaPublicacion(lib.getFechaPublicacion());
+        libro1.setEditorial(lib.getEditorial());
+        libro1.setIsbn(lib.getIsbn());
+
+        listaLibros.set(idPosicion, libro1);
+        return libro1;
+    }
+
 }
