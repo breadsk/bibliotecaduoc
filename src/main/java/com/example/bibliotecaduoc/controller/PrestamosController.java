2ee345afcea79f31be5f503c0bf7c079772568b9
Nicolas <nicolas.programador@gmail.com>
2025-02-16 23:19:45 -0300

    Retoque en controlador

--- src/main/java/com/example/bibliotecaduoc/controller/PrestamosController.java
+++ src/main/java/com/example/bibliotecaduoc/controller/PrestamosController.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class PrestamosController {
 
     private final PrestamoService prestamoService;
-
+    
     public PrestamosController(PrestamoService prestamoService){
         this.prestamoService = prestamoService;
     }
